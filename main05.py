506740d15b7398ef335a95da0b384c7713ef4c5f
joaosantanared <jvtreloso@gmail.com>
2025-04-29 15:03:26 -0300

    first commit

---
+++ main05.py
@@ -0,0 +1,6 @@
+nomes=["joão","Carlos", "Maria", "luiza", "isabel"]
+NomeCerto = input("Digite o nome de um aluno")
+
+for i in range(len(NomeCerto)):
+    if nomes[i] == NomeCerto:
+        print(f"o nome é{NomeCerto}, e está na lista de posição {i}")
